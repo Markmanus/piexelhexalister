15581a10d4e04ebcc718b7066c7c203cd1c619f3
0xcircuitbreaker <sarris88p@gmail.com>
2023-07-10 16:12:08 -0400

    add requirements.txt, revise output to list unique hexs only, revised output to be used with the on-chain nft smart contract, allow to point the script instead of manually typing the files into the main.py

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+import sys
+import os
 from pixel import PixelArtConverter
-from PIL import Image
-from pandas import DataFrame
 
-# Example usage
-picture_paths = ["pixelart.png", "pixelart2.png"]
+# Get command line argument
+path = sys.argv[1]
+
+# If path is a directory, get all files in the directory.
+# If path is a file, use the file.
+if os.path.isdir(path):
+    picture_paths = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+else:
+    picture_paths = [path]
+
+# Convert all images
 converter = PixelArtConverter(picture_paths)
 converter.convert_to_hex()
 
-for i, pixels in enumerate(converter.pixel_data):
-    print(f"Picture {i+1}:")
-    for pixel in pixels:
-        print(f"Coordinates: ({pixel['x']}, {pixel['y']}) - Color: {pixel['color']}")
+# Set to hold all unique colors
+all_unique_colors = set()
 
+for i, pixels in enumerate(converter.pixel_data):
+    unique_colors = set()
+    for pixel in pixels:
+        unique_colors.add(pixel['color'])
+        all_unique_colors.add(pixel['color'])
+
+    print(f"Picture {i+1} ({picture_paths[i]}):")
+    print("Hex Codes used:")
+    for color in unique_colors:
+        print(color)
+
+# Map all unique colors to .c## format and concatenate
+unique_colors_str = "".join(f".c{i:02d}{{fill:{color}}}" for i, color in enumerate(sorted(list(all_unique_colors))))
+
+print("All unique colors:")
+print(unique_colors_str)
