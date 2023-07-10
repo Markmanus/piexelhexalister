0c1a30a336f25cd078bf005dfca0dde7b67219a6
mo0nkn1ght <109871736+mo0nkn1ght@users.noreply.github.com>
2023-07-10 15:43:37 -0400

    add requirements.txt, fix output to only show unique hex codes, added feature to point main.py at either files or an entire dir to mass scan

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import sys
+import os
 from pixel import PixelArtConverter
-from PIL import Image
-from pandas import DataFrame
 
-# Example usage
-picture_paths = ["pixelart.png", "pixelart2.png"]
+# Get command line argument
+path = sys.argv[1]
+
+# If path is a directory, get all files in the directory.
+# If path is a file, use the file.
+if os.path.isdir(path):
+    picture_paths = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+else:
+    picture_paths = [path]
+
+# Convert all images
 converter = PixelArtConverter(picture_paths)
 converter.convert_to_hex()
 
 for i, pixels in enumerate(converter.pixel_data):
-    print(f"Picture {i+1}:")
+    unique_colors = set()
     for pixel in pixels:
-        print(f"Coordinates: ({pixel['x']}, {pixel['y']}) - Color: {pixel['color']}")
+        unique_colors.add(pixel['color'])
 
+    print(f"Picture {i+1} ({picture_paths[i]}):")
+    print("Hex Codes used:")
+    for color in unique_colors:
+        print(color)
